65cd47e4907879c27b41e1abf706624a053c0a99
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2023-12-14 21:01:25 -0500

    Creando Update Class service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,8 @@ const routes:Routes = [
   {path:'', redirectTo:'incio', pathMatch:'full'},
   {path:'inicio',component:InicioComponent},
   {path:'clientes', component:ClientesComponent},
-  {path:'clientes/form', component:FormComponent}
+  {path:'clientes/form', component:FormComponent},
+  {path:'clientes/form/:id', component:FormComponent}
 ];
 
 @NgModule({
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -13,17 +14,37 @@ export class FormComponent implements OnInit{
   cliente:Cliente = new Cliente();
   public titulo:string = "Crear Usuario";
 
-  constructor(private clienteServices: ClienteService, private router:Router){}
+  constructor(private clienteService: ClienteService, private router:Router, private activatedRouter: ActivatedRoute){}
 
   ngOnInit(): void {  
+    this.cargarCliente();
+  }
+
+  cargarCliente(){
+    this.activatedRouter.params.subscribe(params =>{
+      let id = params ['id']
+      if(id){
+        this.clienteService.getCliente(id).subscribe((cliente) => this.cliente = cliente)
+      }
+    })
   }
 
   create(): void{
-    //console.log("Click");
+    //console.log("click");
     //console.log(this.cliente);
-    this.clienteServices.create(this.cliente).subscribe(
-      response => this.router.navigate(['/clientes'])
-    );
-  }
+    this.clienteService.create(this.cliente).subscribe(
+      cliente => {
+        this.router.navigate(['/clientes'])
+        Swal.fire('Nuevo Cliente',`Cliente ${ cliente.nombre } creado con éxito!`, 'success')
+      }
+    )
+}
+
+update():void{
+  this.clienteService.update(this.cliente).subscribe(cliente=>{
+    this.router.navigate(['/clientes'])
+    Swal.fire('Cliente Actualizado', `Cliente ${cliente.nombre} actualizado con éxito`, 'success')
+  })
+}
 
 }
