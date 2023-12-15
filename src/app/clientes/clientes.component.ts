7403255612b612aefcb01b9d96c3a221b0507102
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2023-12-14 21:09:55 -0500

    Delete metodo

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import { Observable } from 'rxjs';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-clientes',
@@ -17,6 +20,43 @@ export class ClientesComponent implements OnInit{
     this.clienteServices.getAll().subscribe(
       clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente:Cliente):void{
+    
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Esta seguro?",
+      text: `Seguro que desea eliminar al cliente ${(cliente.nombre)}?`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Si, Eliminar!",
+      cancelButtonText: "No, cancelar!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+  
+        this.clienteServices.delete(cliente.id).subscribe(
+          response => {
+  
+            this.clientes = this.clientes.filter(cli => cli !== cliente) 
+  
+            swalWithBootstrapButtons.fire({
+              title: "Cliente Eliminado",
+              text: `Cliente ${(cliente.nombre)} Eliminado con Éxito.`,
+              icon: "success"
+            });
+          }
+        )
+      } 
+    });
+  
   }
 
 }
